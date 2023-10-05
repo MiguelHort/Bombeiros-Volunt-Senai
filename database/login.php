916980b7b680b13132cb0d31b9e33a41f14ed9c2
heitork1 <heitorklein70@gmail.com>
2023-10-05 11:31:36 -0300

    atualização login.php e hover no cadastro e login

--- database/login.php
+++ database/login.php
@@ -2,10 +2,10 @@
 session_start();
 include("conexao.php");
 
-$user_email = mysqli_real_escape_string($conexao, trim($_POST['email']));
+$user_equipe = mysqli_real_escape_string($conexao, trim($_POST['equipe']));
 $user_password = mysqli_real_escape_string($conexao, trim(md5($_POST['password'])));
 
-$sql = "SELECT * FROM usuarios WHERE email = '$user_email' AND password = '$user_password'";
+$sql = "SELECT * FROM equipe_atendimento WHERE equipe = '$user_equipe' AND password = '$user_password'";
 $result = mysqli_query($conexao, $sql);
 $row = mysqli_fetch_assoc($result);
 $count = mysqli_num_rows($result);
@@ -14,12 +14,11 @@ if ($count == 1) {
     // As credenciais são válidas, o usuário está autenticado
     $_SESSION['usuario_id'] = $row['id']; // Salvar o ID do usuário na sessão, se necessário
     $_SESSION['usuario_nome'] = $row['name']; // Salvar o nome do usuário na sessão, se necessário
-
-    header('Location: onlogin.html'); // Redirecionar para a página do painel do usuário
+    header('Location: ocorrencia.html'); // Redirecionar para a página do painel do usuário
     exit;
 } else {
     $_SESSION['login_invalido'] = true; // Credenciais inválidas
-    header('Location: offlogin.html'); // Redirecionar de volta para a página de login
+    header('Location: index.html'); // Redirecionar de volta para a página de login
     exit;
 }
 

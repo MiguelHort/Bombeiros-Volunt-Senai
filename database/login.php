ac2ae20b8a3db9abc1a1b7853ae9be10fd0265e5
MiguelHort <you@example.com>
2023-10-10 13:24:17 -0300

    Login funcionando

--- database/login.php
+++ database/login.php
@@ -1,44 +1,27 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 session_start();
-
 include("conexao.php");
 
 $user_equipe = mysqli_real_escape_string($conexao, trim($_POST['equipe']));
-$user_password = trim($_POST['password']);
+$user_password = mysqli_real_escape_string($conexao, trim($_POST['password']));
 
-// Preparar a consulta SQL usando uma declaração preparada
-$sql = "SELECT * FROM equipe_atendimento WHERE equipe = ?";
-
-$stmt = $conexao->prepare($sql);
-$stmt->bind_param("s", $user_equipe);
-$stmt->execute();
-$stmt->store_result();
-$count = $stmt->num_rows;
+$sql = "SELECT * FROM equipe_atendimento WHERE equipe = '$user_equipe' AND senha = '$user_password'";
+$result = mysqli_query($conexao, $sql);
+$row = mysqli_fetch_assoc($result);
+$count = mysqli_num_rows($result);
 
 if ($count == 1) {
-    // Obter a senha hash do banco de dados
-    $stmt->bind_result($id, $equipe, $senha);
-    $stmt->fetch();
+    // As credenciais são válidas, o usuário está autenticado
+    $_SESSION['usuario_id'] = $row['id']; // Salvar o ID do usuário na sessão, se necessário
+    $_SESSION['usuario_nome'] = $row['name']; // Salvar o nome do usuário na sessão, se necessário
 
-    // Verificar a senha usando password_verify()
-    if (password_verify($user_password, $senha)) {
-        $_SESSION['usuario_id'] = $id;
-        $_SESSION['usuario_equipe'] = $equipe;
-        header('Location: ../pages/ocorrencia.html');
-        exit;
-    } else {
-        $_SESSION['login_invalido'] = true;
-        header('Location: ../index.html');
-        exit;
-    }
+    header('Location: ../pages/ocorrencia.html'); // Redirecionar para a página do painel do usuário
+    exit;
 } else {
-    $_SESSION['login_invalido'] = true;
-    header('Location: ../index.html');
+    $_SESSION['login_invalido'] = true; // Credenciais inválidas
+    header('Location: ../pages/cadastro.html'); // Redirecionar de volta para a página de login
     exit;
 }
 
-$stmt->close();
 $conexao->close();
 ?>

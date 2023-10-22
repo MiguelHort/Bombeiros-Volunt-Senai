bf73c708be6b2853635eacad77079865304a790a
heitork1 <heitorklein70@gmail.com>
2023-10-22 17:04:56 -0300

    80% histórico

--- pages/historico.php
+++ pages/historico.php
@@ -11,14 +11,29 @@
 </head>
 
 <body>
+<p id="historico">HISTÓRICO</p>
      <div class="accordion" id="accordionPanelsStayOpenExample">
         <?php
         error_reporting(E_ALL);
         ini_set('display_errors', 1);
         include('../database/conexao.php'); // Inclua o arquivo de conexão
+        mysqli_set_charset($conexao, "utf8");
 
         // Consulta SQL para obter os dados da ocorrência com id=1
-        $sql = "SELECT * FROM ocorrencia INNER JOIN paciente on ocorrencia.fk_paciente = paciente.id_paciente";
+        $sql = "SELECT * FROM ocorrencia 
+INNER JOIN paciente on ocorrencia.fk_paciente = paciente.id_paciente
+INNER JOIN acompanhante on paciente.fk_acompanhante = acompanhante.id_acompa
+INNER JOIN anamnese_emerg on paciente.fk_anamnese_emerg = anamnese_emerg.id_anamnese_emerg
+LEFT JOIN anamnese_gest on paciente.fk_anamnese_gest = anamnese_gest.id_anamnese_gest
+INNER JOIN avaliacao on paciente.fk_avaliacao = avaliacao.id_avaliacao
+INNER JOIN sinais_vitais on paciente.fk_sinais_vitais = sinais_vitais.id_sinais_vitais
+INNER JOIN problem_suspeito on paciente.fk_problem_suspeito = problem_suspeito.id_problem_suspeito
+INNER JOIN localizacao_trauma on paciente.fk_localizacao_trauma = localizacao_trauma.id_localizacao_trauma
+INNER JOIN sinais_sintomas on paciente.fk_sinais_sintomas = sinais_sintomas.id_sinais_sintomas
+INNER JOIN proced_efetuados on paciente.fk_proced_efetuados = proced_efetuados.id_proced_efetuados
+INNER JOIN cinematica on paciente.fk_cinematica = cinematica.id_cinematica
+INNER JOIN equipe_atendimento on ocorrencia.fk_equipe_atendimento = equipe_atendimento.id_equipe_atendimento
+INNER JOIN info_finais on ocorrencia.fk_info_finais = info_finais.id_info_finais;";
         $result = mysqli_query($conexao, $sql);
 
         if ($result) {
@@ -40,239 +55,215 @@
                             <!-- Coloque aqui as informações específicas da ocorrência usando $row['nome_do_campo'] -->
                             <h2>Informações (vítima e ocorrência)</h2>
                     <div><strong>Nome da vítima:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['nome']; ?></p>
                     </div>
                     <div><strong>CPF:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['rg_cpf']; ?></p>
                     </div>
                     <div><strong>SEXO:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['sexo']; ?></p>
                     </div>
                     <div><strong>NOME DO ACOMPANHANTE:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['nome_acompa']; ?></p>
                     </div>
                     <div><strong>RELAÇÃO COM ACOMPANHANTE:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['relacao']; ?></p>
                     </div>
                     <div><strong>O QUE ACONTECEU:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['oque_aconteceu']; ?></p>
                     </div>
                     <div><strong>ACONTECEU OUTRAS VEZES:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['outras_vezes']; ?></p>
                     </div>
                     <div><strong>A QUANTO TEMPO ACONTECEU:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['quando_aconteceu']; ?></p>
                     </div>
                     <div><strong>POSSUI ALGUM PROBLEMA DE SAÚDE:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['problema_de_saude']; ?></p>
                     </div>
                     <div><strong>QUAIS PROBLEMAS:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['quais_problemas']; ?></p>
                     </div>
                     <div><strong>FAZ USO DE MEDICAÇÃO:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['usa_medicacao']; ?></p>
+                    </div>
+                    <div><strong>HORÁRIO DA ÚLTIMA MEDICAÇÃO:</strong>
+                        <p><?php echo $row['horario_medicacao']; ?></p>
                     </div>
                     <div><strong>QUAIS:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['quais_medicacoes']; ?></p>
                     </div>
                     <div><strong>ALÉRGICO A ALGO:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['alergico']; ?></p>
                     </div>
                     <div><strong>ALÉRGICO A:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['qual_alergia']; ?></p>
                     </div>
 
                     <div><strong>GESTANTE:</strong>
-                        <p>?</p>
+                        <p><?php if( empty($row['fk_anamnese_gest']))
+                        {
+                            echo "Não";
+                        } else {
+                            echo "Sim";
+                        }  ?></p>
                     </div>
                     <div><strong>PRÉ-NATAL:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['pre_natal']; ?></p>
                     </div>
                     <div><strong>PERÍODO DA GESTAÇÃO:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['periodo_gest']; ?></p>
                     </div>
                     <div><strong>NOME DO MÉDICO:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['nome_medico']; ?></p>
                     </div>
                     <div><strong>POSSÍVEIS COMPLICAÇÕES:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['complicacao']; ?></p>
                     </div>
                     <div><strong>PRIMERO FILHO:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['primeiro_filho']; ?></p>
                     </div>
                     <div><strong>QUANTOS:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['quantos']; ?></p>
                     </div>
                     <div><strong>CONTRAÇÕES:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['contracoes']; ?></p>
                     </div>
                     <div><strong>QUE HORAS INICIARAM AS CONTRAÇÕES:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['horario_contracoes']; ?></p>
                     </div>
                     <div><strong>INTERVALO ENTRE CONTRAÇÕES:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['intervalo_contracoes']; ?></p>
                     </div>
                     <div><strong>DURAÇÃO DAS CONTRAÇÕES:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['duracao_contracoes']; ?></p>
                     </div>
                     <div><strong>PRESSÃO NO QUADRIL OU VONTADE DE EVACUAR:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['pressao_ou_evacuar']; ?></p>
                     </div>
                     <div><strong>JÁ HOUVE RUPTURA NA BOLSA?:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['ruptura_bolsa']; ?></p>
                     </div>
                     <div><strong>FOI FEITA INSPEÇÃO VISUAL?::</strong>
-                        <p>?</p>
+                        <p><?php echo $row['inspecao_visual']; ?></p>
                     </div>
-                    <div><strong>PARTO REALIZADO?::</strong>
-                        <p>?</p>
+                    <div><strong>PARTO REALIZADO?:</strong>
+                        <p><?php echo $row['parto_realizado']; ?></p>
                     </div>
                     <div><strong>SEXO DO BEBÊ:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['sexo_bebe']; ?></p>
                     </div>
 
                     <div><strong>TIPO DE OCORRÊNCIA:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['tipo_ocorrencia']; ?></p>
                     </div>
                     <div><strong>AVALIAÇÃO GLASGOW:</strong>
-                        <p>?</p>
-                    </div>
-                    <div><strong>GESTANTE:</strong>
-                        <p>?</p>
-                    </div>
-                    <div><strong>GESTANTE:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['total_gcs']; ?></p>
                     </div>
 
                     <div><strong>PRESSÃO ARTERIAL:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['mmHg']; ?></p>
                     </div>
                     <div><strong>PULSO:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['bcpm']; ?></p>
                     </div>
                     <div><strong>RESPIRAÇÃO:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['respiracao']; ?></p>
                     </div>
                     <div><strong>SATURAÇÃO:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['saturacao']; ?></p>
                     </div>
                     <div><strong>HGT:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['hgt']; ?></p>
                     </div>
                     <div><strong>TEMPERATURA:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['temperatura']; ?></p>
                     </div>
 
-                    <div><strong>PROBLEMAS PSIQUIÁTRICOS:</strong>
-                        <p>?</p>
+                    <div><strong>PROBLEMAS SUSPEITOS:</strong>
+                        <p><?php echo $row['tipo_problem']; ?></p>
                     </div>
-                    <div><strong>PROBLEMAS RESPIRATÓRIOS:</strong>
-                        <p>?</p>
-                    </div>
-                    <div><strong>DIABETES:</strong>
-                        <p>?</p>
-                    </div>
-                    <div><strong>OUTROS:</strong>
-                        <p>?</p>
-                    </div>
+        
 
                     <div><strong>LOCALIZAÇÃO E TIPO DE TRAUMA:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['localizacao_aprox']; ?>, <?php echo $row['tipo_trauma']; ?></p>
                     </div>
                     <div><strong>SINAIS E SINTOMAS:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['sintomas']; ?></p>
                     </div>
 
                     <div><strong>FORMA DE CONDUÇÃO:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['forma_conducao']; ?></p>
                     </div>
                     <div><strong>VÍTIMA ERA:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['vitima_era']; ?></p>
                     </div>
                     <div><strong>DECISÃO TRANSPORTE:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['decisao_transp']; ?></p>
                     </div>
                     <div><strong>PROCEDIMENTOS EFETUADOS:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['procedimentos']; ?></p>
                     </div>
                     <div><strong>MATERIAIS UTILIZADOS DESCARTÁVEIS:</strong>
-                        <p>?</p>
+                        <p></p>
                     </div>
                     <div><strong>MATERIAIS UTILIZADOS DEIXADOS NO HOSPITAL:</strong>
                         <p>?</p>
                     </div>
 
 
-                    <div><strong>ENCONTRADO DE CAPACETE:</strong>
-                        <p>?</p>
-                    </div>
-                    <div><strong>DISTÚRBIO DE COMPORTAMENTO:</strong>
-                        <p>?</p>
-                    </div>
-                    <div><strong>ENCONTRADO DE CINTO:</strong>
-                        <p>?</p>
-                    </div>
-                    <div><strong>PARA-BRISAS AVARIADO:</strong>
-                        <p>?</p>
-                    </div>
-                    <div><strong>CAMINHANDO NA CENA:</strong>
-                        <p>?</p>
-                    </div>
-                    <div><strong>PAINEL AVARIADO:</strong>
-                        <p>?</p>
-                    </div>
-                    <div><strong>VOLANTE TORCIDO:</strong>
-                        <p>?</p>
+                    <div><strong>AVALIAÇÃO DA CINEMÁTICA:</strong>
+                        <p><?php echo $row['avaliacao_cinematica']; ?></p>
                     </div>
 
                     <div><strong>OBJETOS RECOLHIDOS:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['objetos_recolhidos']; ?></p>
                     </div>
                     <div><strong>OBSERVAÇÕES IMPORTANTES:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['obs_importantes']; ?></p>
                     </div>
                     <div><strong>M:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['m']; ?></p>
                     </div>
                     <div><strong>S1:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['s1']; ?></p>
                     </div>
                     <div><strong>S2:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['s2']; ?></p>
                     </div>
                     <div><strong>S3:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['s3']; ?></p>
                     </div>
                     <div><strong>DEMANDANTE:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['demandante']; ?></p>
                     </div>
                     <div><strong>EQUIPE:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['equipe']; ?></p>
                     </div>
                     <div><strong>Nº USB:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['usb']; ?></p>
                     </div>
                     <div><strong>Nº OCORRÊNCIA:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['id_ocorrencia']; ?></p>
                     </div>
 
                     <div><strong>CÓD. IR:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['ir']; ?></p>
                     </div>
                     <div><strong>CÓD. PS:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['ps']; ?></p>
                     </div>
                     <div><strong>DESP:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['desp']; ?></p>
                     </div>
                     <div><strong>KM FINAL:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['km_final']; ?></p>
                     </div>
                     <div><strong>H. CH:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['h_ch']; ?></p>
                     </div>
                     <div><strong>SIA/SUS:</strong>
-                        <p>?</p>
+                        <p><?php echo $row['sia_sus']; ?></p>
                     </div>
                         </div>
                     </div>

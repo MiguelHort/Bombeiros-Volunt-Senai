3d9662ed46ea0e6524ebb32ca61b1dcc4217cb42
Miguel Hort <miguel.d.hort@gmail.com>
2023-11-28 12:23:59 -0300

    Arrumando Admin

--- database/conexao.php
+++ database/conexao.php
@@ -2,9 +2,9 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-define('HOST', 'sql310.infinityfree.com');
-define('USUARIO', 'if0_35105772');
-define('SENHA', '4mgQ0lECgAW9oVT');
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', 'root');
 define('DB', 'if0_35105772_ocorrencias');
  
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
